9dcba9ead293b77072375272fcecdf7dd05a0c63
Aryan Aggarwal <aryan.agg72@gmail.com>
2026-08-07 06:05:17 +0530

    Integrate Neo4j and refactor database connection
    Refactor database connection to use Neo4j and add connectivity verification.

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,14 +1,29 @@
-const mongoose = require("mongoose")
+const neo4j = require("neo4j-driver")
+const dotenv = require("dotenv")
 
-async function connectDb(){
+dotenv.config();
 
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
+const driver = neo4j.driver(
+    process.env.COGNODB_URI,
+    neo4j.auth.basic(
+        process.env.COGNODB_USERNAME,
+        process.env.COGNODB_PASSWORD
+    )
+);
 
-        console.log("Database connect successfully")
-    }catch(err){
-        console.log("Database connection error: ", err)
+const connectCognoDB = async () => {
+    try {
+        await driver.verifyConnectivity();
+        console.log("Connected to CognoDB Successfully...");
     }
-}
+    catch (error) {
+        console.error("CognoDB Connection Failed.");
+        console.error(error.message);
+        process.exit(1);
+    }
+};
 
-module.exports = connectDb
+module.exports = { 
+    driver,
+    connectCognoDB
+}
